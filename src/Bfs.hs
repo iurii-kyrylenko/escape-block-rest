be2fba94e851da4c9bb1bdf3f7859991cf2af883
Iurii Kyrylenko <iurii.kyrylenko@altexsoft.com>
2017-11-30 12:45:06 +0200

    Data.HashSet -> Data.Set

--- src/Bfs.hs
+++ src/Bfs.hs
@@ -5,12 +5,12 @@ module Bfs(
 
 {- http://aleph.nz/post/search_in_haskell/ -}
 
-import           Data.Hashable
-import qualified Data.HashSet  as S
+-- import           Data.Hashable
+import qualified Data.Set  as S
 import qualified Data.Sequence as Q
 import           Data.List (unfoldr)
 
-bfs :: (Hashable a, Eq a, Ord a)
+bfs :: (Eq a, Ord a)
   => (a -> [a])
   -> a
   -> [(a,a)]
@@ -19,17 +19,17 @@ bfs adj start = bfs' adj seen queue
     seen  = S.singleton start
     queue = Q.singleton (start, start)
 
-bfs' :: (Hashable a, Eq a, Ord a)
+bfs' :: (Eq a, Ord a)
   => (a -> [a])
-  -> S.HashSet a
+  -> S.Set a
   -> Q.Seq (a,a)
   -> [(a,a)]
 bfs' adj seen queue = unfoldr (bfs_step adj) (seen, queue)
 
-bfs_step :: (Hashable a, Eq a, Ord a)
+bfs_step :: (Eq a, Ord a)
   => (a -> [a])
-  -> (S.HashSet a, Q.Seq (a,a))
-  -> Maybe ((a,a), (S.HashSet a, Q.Seq (a,a)))
+  -> (S.Set a, Q.Seq (a,a))
+  -> Maybe ((a,a), (S.Set a, Q.Seq (a,a)))
 bfs_step neighbours (seen, queue)
   | Q.null queue = Nothing
   | otherwise    = Just ((parent, current), next)
@@ -40,7 +40,7 @@ bfs_step neighbours (seen, queue)
       queue' = remaining Q.>< (Q.fromList . map ((,) current) . S.toList) descendents
       descendents = S.difference (S.fromList $ neighbours current) seen
 
-bfsBacktrack :: (Hashable a, Eq a, Ord a)
+bfsBacktrack :: (Eq a, Ord a)
   => (a -> [a])
   -> (a -> Bool)
   -> a
